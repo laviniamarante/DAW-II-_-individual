f289dd198599bd4684e71e5291ebd6fd19dd790a
laviniamarante <laviinia.amarante@gmail.com>
2026-03-24 10:05:58 -0300

    "Alterações"

--- src/app/agendamento/page.js
+++ src/app/agendamento/page.js
@@ -1,5 +1,12 @@
-export default () => (
-    <div>
-        <h1>Agendamento</h1>
-    </div>
-);
+import db from '@/lib/db';
+export default async () => {
+  const consumidores = await db.query("select * from consumidor ")
+return (<><h1>Consumidores</h1>
+<ol>
+    {consumidores.map(c => <li>{c.nome}</li>)}
+    <li></li>
+    </ol>
+</>
+)
+}
+
